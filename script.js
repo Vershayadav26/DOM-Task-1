68948880fc688ff4cc8056c775c533d0d6fd00dd
Versha Yadav <vershayadav264@gmail.com>
2026-01-12 20:53:17 +0530

    Initial commit

--- script.js
+++ script.js
@@ -1,67 +1,24 @@
-var btn = document.querySelector('button')
+var img = document.querySelector('.card img');
+var love = document.querySelector('.card i');
 
-var main = document.querySelector('main')
-var arr = ['Hey ! I am Versha ','sheryians is Best','sarthak bhaiya is Best','Harsh bhaiya is Best']
- btn.addEventListener('click',function(){
-var h1 = document.createElement('h1')
-var x = Math.random()*80
-var y = Math.random()*80
-var rot = Math.random()*360
-var scl = Math.random()*4
-var a = Math.floor(Math.random()*arr.length)
+img.addEventListener('dblclick', function () {
 
-h1.innerHTML = arr[a]
-h1.style.position = 'absolute'
+  // show heart
+  love.style.opacity = "1";
+  love.style.transform = "translate(-50%, -50%) scale(1)";
 
-h1.style.left = x+'%'
-h1.style.top = y+'%'
-h1.style.rotate = rot+'deg'
-h1.style.scale = scl
+  // thoda upar fly effect
+  setTimeout(function () {
+    love.style.transform = "translate(-50%, -300%) scale(1)";
+  }, 600);
 
-main.appendChild(h1)
+  // hide heart smoothly
+  setTimeout(function () {
+    love.style.opacity = "0";
+    love.style.transform = "translate(-50%, -50%) scale(0)";
+  }, 1200);
 
-//     var div = document.createElement('div')
-
-//      var x = Math.random()*100
-//     var y = Math.random()*100
-//     var r = Math.random()*360
-//      var c1 = Math.floor(Math.random()*256)
-//        var c2 = Math.floor(Math.random()*256)
-//          var c3 = Math.floor(Math.random()*256)
-
-
-//     div.style.height = '50px'
-//     div.style.width = '50px'
-//      div.style.position = 'absolute'
-//      div.style.backgroundColor = `rgb(${c1},${c2},${c3})`
-
-//      div.style.left = x+'%'
-//      div.style.top = y+'%'
-//       div.style.rotate = r+ 'deg'
-
-//     main.appendChild(div)
-})
-
-
-// var btn = document.createElement('button')
-
-// btn.innerHTML= 'download'
-
-// btn.style.color ='red'
-// var main = document.querySelector('main')
-
-// main.appendChild (btn)
+});
 
 
 
-
-
-
-
-
-//     var h1 = document.createElement('h1')
-
-//     h1.innerHTML = 'Hello From js'
-//   var main =  document.querySelector('main')
-
-// main.appendChild(h1)
