00dbb0e23ce9917653c6fc27e36a2fac15fd115e
Versha Yadav <vershayadav264@gmail.com>
2026-01-20 23:31:57 +0530

    add friend

--- day-49/script.js
+++ day-49/script.js
@@ -1,13 +1,10 @@
-var aud = new Audio('./31.mp3');
-var h1 = document.querySelector('h1');
-
-document.body.addEventListener('keydown', function(dets) {
-    if (dets.code == 'keyD') {
-        aud.play();
-        
-    }
-      if (dets.code == 'keyJ') {
-        aud.play();
-        
-    }
+var allBtn = document.querySelectorAll('buttton');
+allBtn.forEach(function(elem){
+    elem.addEventListener('click', function(){
+        if(elem.innerHTML == 'Add Friend'){
+            elem.innerHTML == 'Remove Friend'
+        } else {    
+            elem.innerHTML == 'Add Friend'
+        }
+    })
 })
